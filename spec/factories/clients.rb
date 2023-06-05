be66e466ce63ed9e647627646c9fa39f0147cb29
Williams Colmenares <david.13.2121@gmail.com>
2023-06-05 19:43:37 -0400

    add factories for clients

--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :client do
-    
+    name { Faker::Name.name }
+    last_name { Faker::Name.last_name }
+    address { Faker::Address.full_address }
+    phone { Faker::PhoneNumber.cell_phone_in_e164 }
   end
 end
