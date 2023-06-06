69684ea322c5525a190826c02f03b9b4e4e73e5c
Williams Colmenares <david.13.2121@gmail.com>
2023-06-06 17:07:42 -0400

    add new validations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  enum role: { admin: 0, agent: 1, customer: 2 }
+  enum role: { admin: 0, agent: 1, client: 2 }
 
-  validates :email, presence: true
-  validates :password, presence: true
+  validates_presence_of :email, :password, :role
+  validates_uniqueness_of :email
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
+  validates :role, inclusion: { within: %w[client agent admin] }
+  validates :password, length: { maximum: 72, minimum: 8 }, confirmation: true
 end
