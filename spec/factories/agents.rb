1e1b3f1b76ee630db149b8056b18a7a00f9e687a
Williams Colmenares <david.13.2121@gmail.com>
2023-06-05 20:47:10 -0400

    add factory to property

--- spec/factories/agents.rb
+++ spec/factories/agents.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :agent do
-    
+    name { Faker::Name.name }
+    last_name { Faker::Name.last_name }
+    phone { Faker::PhoneNumber.cell_phone_in_e164 }
+    address { Faker::Address.full_address }
+    agency { Faker::Lorem.sentence(word_count: 3) }
+    avatar { Faker::Avatar.image(size: '50x50') }
   end
 end
--- spec/factories/properties.rb
+++ spec/factories/properties.rb
@@ -1,5 +1,27 @@
 FactoryBot.define do
   factory :property do
-    
+    title { Faker::Lorem.sentence(word_count: 4) }
+    description { Faker::Lorem.sentence(word_count: 50) }
+    direction { Faker::Address.full_address }
+    price { Faker::Commerce.price(range: 1000.0..10_000_000.0) }
+    area  { Faker::Number.decimal(l_digits: 3, r_digits: 2).to_f }
+
+    mode { rand(0..1).round }
+    type { rand(0..4).round }
+    state { rand(0..3).round }
+
+    qty_bedroom { rand(0..30).round }
+    qty_bathroom { rand(0..30).round }
+    qty_floor { rand(0..30).round }
+    qty_kitchen { rand(0..10).round }
+    qty_parking { rand(0..30).round }
+    qty_hall { rand(0..10).round }
+
+    send(:private) { Faker::Boolean.boolean }
+    office { Faker::Boolean.boolean }
+    shop { Faker::Boolean.boolean }
+    yard { Faker::Boolean.boolean }
+    garden { Faker::Boolean.boolean }
+    social { Faker::Boolean.boolean }
   end
 end
