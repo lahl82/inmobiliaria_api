0b25485139d4201cca7ced40edf7fca57fafeafc
Williams Colmenares <david.13.2121@gmail.com>
2023-06-21 19:20:31 -0400

    city validations and specs

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,5 @@
 class City < ApplicationRecord
-    has_many :zones
+  has_many :zones
+
+  validates :name, uniqueness: true, presence: true, length: { in: 3..30 }, format: { with: /\A[a-zA-Z]+\z/ }
 end
--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe City, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'City factory must be valid' do
+    city = build(:city)
+    expect(city).to be_valid
+  end
+
+  describe 'validations' do
+    subject { build(:city) }
+
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_most(30) }
+    it { should validate_length_of(:name).is_at_least(3) }
+    it { should validate_uniqueness_of(:name) }
+    it { should_not allow_value('San Cristobal 123').for(:name) }
+  end
 end
