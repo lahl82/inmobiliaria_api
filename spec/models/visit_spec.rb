3c80dfc3c50533435d7ae3a60922de8c5e29f72f
Williams Colmenares <david.13.2121@gmail.com>
2023-06-22 22:18:52 -0400

    add custom validation for visits, and finalize the specs

--- spec/models/visit_spec.rb
+++ spec/models/visit_spec.rb
@@ -1,33 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Visit, type: :model do
-  # describe 'validations' do
-  #   it { should validate_presence_of(:date) }
+  describe 'validations' do
+    it { should validate_presence_of(:date) }
 
-  #   it 'validates the format of date' do
-  #     visit = FactoryBot.build(:visit, date: '2023-06-22')
-  #     expect(visit).to be_valid
+    it 'validates the format of date' do
+      visit = build(:visit, date: '22/06/2023') # Invalid format
+      expect(visit).not_to be_valid
+    end
 
-  #     visit.date = '22/06/2023' # Invalid format
-  #     expect(visit).not_to be_valid
-  #   end
+    it 'validates the inclusion of date within a range' do
+      visit = build(:visit, date: Date.current + 15.days)
+      expect(visit).to be_valid
 
-  #   it 'validates the inclusion of date within a range' do
-  #     visit = FactoryBot.build(:visit, date: Date.current + 15.days)
-  #     expect(visit).to be_valid
+      visit.date = Date.current - 1.day # Outside the range
+      expect(visit).not_to be_valid
 
-  #     visit.date = Date.current - 1.day # Outside the range
-  #     expect(visit).not_to be_valid
-  #   end
+      visit.date = Date.current + 35.day # Outside the range
+      expect(visit).not_to be_valid
+    end
 
-  #   it 'validates that the date is a future date' do
-  #     visit = FactoryBot.build(:visit, date: Date.current + 1.day)
-  #     expect(visit).to be_valid
+    it 'validates that the date is a future date' do
+      visit = build(:visit, date: Date.current + 1.day)
+      expect(visit).to be_valid
 
-  #     visit.date = Date.current # Not a future date
-  #     expect(visit).not_to be_valid
-  #   end
-  # end
+      visit.date = Date.current
+      expect(visit).not_to be_valid
+      expect(visit.errors[:date]).to include('should be a future date')
+    end
+  end
 
   describe 'associations' do
     it { should belong_to(:client) }
