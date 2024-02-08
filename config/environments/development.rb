78bd5f84d9479ccf2c2e7efb69cecf656a30bfbb
lahl82 <luisadolfo.hernandezlara@gmail.com>
2024-02-08 17:59:40 -0400

    added a valid host to the development configuration environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
+
+  config.hosts << '6496-143-255-84-121.ngrok-free.app'
 end
