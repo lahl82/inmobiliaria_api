2cd5b807edc33a637da0a87c87d0734d20d8caa0
lahl82 <luisadolfo.hernandezlara@gmail.com>
2024-03-12 13:20:37 -0400

    delete unnecessary old code

--- app/serializers/user_serializer.rb
+++
@@ -1,8 +0,0 @@
-class UserSerializer
-  include JSONAPI::Serializer
-  attributes :id, :email, :role, :created_at
-
-  attribute :created_date do |user|
-    user.created_at&.strftime('%m/%d/%Y')
-  end
-end
